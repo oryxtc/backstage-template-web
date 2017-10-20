0f645ebda1c497a5d67797d25254301528b887d2
oryxtc <oryxtc@gmail.com>
2017-10-20 14:45:28 +0800

    修改主题颜色

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '../theme/index.css'
+
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import '../theme/index.css'
 import App from './App'
 import router from './router'
 
@@ -15,6 +16,6 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App },
+  // components: { App },
   render: h => h(App)
 })
