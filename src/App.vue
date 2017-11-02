baa3fdea1a9d6419ab91ce9bf3d9752aad64c7fb
oryxtc <oryxtc@gmail.com>
2017-11-02 09:59:40 +0800

    更新element-ui至2.0版本

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="app">
         <Sidebar :api-url="this.apiUrl"></Sidebar>
-        <router-view :api-url="this.apiUrl"/>
     </div>
 </template>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,38 +1,35 @@
 <template>
     <div>
-        <!--<el-container style="height: 500px; border: 1px solid #eee">-->
-            <!--<el-aside width="200px" style="background-color: rgb(238, 241, 246)">-->
+        <el-container style=" border: 1px solid #eee">
+            <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
                 <el-menu class="menu-Bar" :collapse="isCollapse">
                     <SidebarTree :menuData="this.menuData"></SidebarTree>
+
                 </el-menu>
-
-            <!--</el-aside>-->
+            </el-aside>
             <el-container>
-                <!--<el-header style="text-align: right; font-size: 12px">-->
-                    <!--<el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">-->
-                        <!--<el-radio-button :label="false">展开</el-radio-button>-->
-                        <!--<el-radio-button :label="true">收起</el-radio-button>-->
-                    <!--</el-radio-group>-->
+                <el-header style="text-align: right; font-size: 12px">
+                    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+                        <el-radio-button :label="false">展开</el-radio-button>
+                        <el-radio-button :label="true">收起</el-radio-button>
+                    </el-radio-group>
 
-                    <!--<el-dropdown>-->
-                        <!--<i class="el-icon-setting" style="margin-right: 15px"></i>-->
-                        <!--<el-dropdown-menu slot="dropdown">-->
-                            <!--<el-dropdown-item>查看</el-dropdown-item>-->
-                            <!--<el-dropdown-item>新增</el-dropdown-item>-->
-                            <!--<el-dropdown-item>删除</el-dropdown-item>-->
-                        <!--</el-dropdown-menu>-->
-                    <!--</el-dropdown>-->
-                    <!--<span>王小虎</span>-->
-                <!--</el-header>-->
+                    <el-dropdown>
+                        <i class="el-icon-setting" style="margin-right: 15px"></i>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>查看</el-dropdown-item>
+                            <el-dropdown-item>新增</el-dropdown-item>
+                            <el-dropdown-item>删除</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <span>王小虎</span>
+                </el-header>
 
-                <!--<el-main>-->
-            <!--1123-->
-                <!--</el-main>-->
+                <el-main>
+                    <router-view :api-url="this.apiUrl"/>
+                </el-main>
             </el-container>
-        <!--</el-container>-->
-
-
-
+        </el-container>
     </div>
 </template>
 <script>
